09d3829e19aee40658e06f1a3c0e3941bebfa5a2
Jorge <jfsawyer88@gmail.com>
2015-10-01 19:46:40 +0000

    Polish notation calculator using stack data structure

---
+++ 3-Hard/007-PrefixExpressions/PrefixExpressions2.py
@@ -0,0 +1,39 @@
+## CodeEval
+## Prefix Expressions
+## This is why I need to learn data structures!
+## using a stack and working backward through
+## the expression will make it much easier
+
+import sys
+
+if 1 != len(sys.argv):
+    f = open(sys.argv[1], 'r')
+else:
+    import os
+    f = open(os.getcwd() + '/test.txt')
+
+ops = '+*/'
+
+for ex in f:
+
+    ex = ex.strip()
+    if ex:
+        ex = ex.split(' ')
+        stack = []
+        for e in reversed(ex):
+            if not e in ops:
+                stack.append(int(e))
+            else:
+                a = stack.pop()
+                b = stack.pop()
+                if e == '+':
+                    stack.append(a+b)
+                if e == '*':
+                    stack.append(a*b)
+                if e == '/':
+                    stack.append(a/b)
+
+        sys.stdout.write(str(stack[0]) + '\n')
+
+
+f.close()
