f4c15a0c65b361780869517b73ef1f953b726d15
Jorge <jfsawyer88@gmail.com>
2015-10-03 04:37:00 +0000

    verifies that the rectangle has equal length sides

--- 2-Moderate/101-FindASquare/FindASquare.py
+++ 2-Moderate/101-FindASquare/FindASquare.py
@@ -11,6 +11,9 @@ else:
     f = open(os.getcwd() + '/test.txt', 'r')
 
 # vector functions
+def dist_sqr(a,b):
+    '''squared distance between two poins'''
+    return (a[0] - b[0])**2 + (a[1] - b[1])**2
 def add(v,w):
     '''vector addition'''
     return [v[0] + w[0], v[1] + w[1]]
@@ -40,7 +43,9 @@ for points in f:
     if i<3:
         b, c = ps[i]
         d = pd[i]
-        is_square =  d == add(a, add(diff(b,a), diff(c,a)))
+        is_square =  d == add(a, add(diff(b,a), diff(c,a)))      # right angles
+        is_square = is_square and dist_sqr(a,b) == dist_sqr(a,c) # equal sides
+        is_square = is_square and a != b                         # not degenerate
 
     sys.stdout.write(str(is_square).lower() + '\n')
 
