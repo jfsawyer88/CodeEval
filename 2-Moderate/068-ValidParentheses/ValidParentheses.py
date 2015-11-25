aeda05571f125a7f568b6dc834462091a4d8f049
jorge <jfsawyer88@gmail.com>
2015-11-25 18:44:10 +0000

    challenge 068

---
+++ 2-Moderate/068-ValidParentheses/ValidParentheses.py
@@ -0,0 +1,32 @@
+## CodeEval
+## Valid Parenthesis
+
+import sys
+
+
+def valid_ex(ex):
+    while ex:
+        p = '([{'.index(ex[0])
+        v = [0, 0, 0]
+        v[p] += 1
+        i = 1
+        while v[p] != 0:
+            if ex[i] in '([{':
+                v['([{'.index(ex[i])] += 1
+            if ex[i] in ')]}':
+                v[')]}'.index(ex[i])] -= 1
+            i += 1
+        if v == [0,0,0]:
+            ex = ex[:(i-1)] + ex[i:]
+            ex = ex[1:]
+        else:
+            return False
+    return True
+
+
+expressions = open(sys.argv[1], 'r')
+
+for ex in expressions:
+    sys.stdout.write(str(valid_ex(ex.strip())) + '\n')
+
+expressions.close()
